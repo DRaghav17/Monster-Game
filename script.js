da3100ec4586760e112f764c862eff547593110b
DRaghav17 <124306020+DRaghav17@users.noreply.github.com>
2023-03-31 14:59:33 +0530

    Update script.js
    Yes

--- script.js
+++ script.js
@@ -9,31 +9,31 @@ setTimeout(() => {
 document.onkeydown = function (e) {
     console.log("Key code is: ", e.keyCode)
     if (e.keyCode == 38) {
-        dino = document.querySelector('.dino');
-        dino.classList.add('animateDino');
+        man = document.querySelector('.man');
+        man.classList.add('animateMan');
         setTimeout(() => {
-            dino.classList.remove('animateDino')
+            dino.classList.remove('animateMan')
         }, 700);
     }
     if (e.keyCode == 39) {
-        dino = document.querySelector('.dino');
-        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
-        dino.style.left = dinoX + 112 + "px";
+        man = document.querySelector('.man');
+        manX = parseInt(window.getComputedStyle(man, null).getPropertyValue('left'));
+        man.style.left = manX + 112 + "px";
     }
     if (e.keyCode == 37) {
-        dino = document.querySelector('.dino');
-        dinoX = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
-        dino.style.left = (dinoX - 112) + "px";
+        man = document.querySelector('.man');
+        manX = parseInt(window.getComputedStyle(man, null).getPropertyValue('left'));
+        man.style.left = (manX - 112) + "px";
     }
 }
 
 setInterval(() => {
-    dino = document.querySelector('.dino');
+    man = document.querySelector('.man');
     gameOver = document.querySelector('.gameOver');
     obstacle = document.querySelector('.obstacle');
 
-    dx = parseInt(window.getComputedStyle(dino, null).getPropertyValue('left'));
-    dy = parseInt(window.getComputedStyle(dino, null).getPropertyValue('top'));
+    dx = parseInt(window.getComputedStyle(man, null).getPropertyValue('left'));
+    dy = parseInt(window.getComputedStyle(man, null).getPropertyValue('top'));
 
     ox = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('left'));
     oy = parseInt(window.getComputedStyle(obstacle, null).getPropertyValue('top'));
